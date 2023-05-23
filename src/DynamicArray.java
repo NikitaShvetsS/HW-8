ff3896e74c8d1dfbb87931aa384f7fb9e46f396e
nikitasvec <cryohush@gmail.com>
2023-05-23 11:50:32 +0200

    hw 8

--- src/DynamicArray.java
+++ src/DynamicArray.java
@@ -1,18 +1,69 @@
 public class DynamicArray {
+    private String[] array;
+    private int size;
 
-    String[] array;
-    int size;
-
-    public DynamicArray(){
+    public DynamicArray() {
         array = new String[10];
         size = 0;
     }
 
-    public String[] getArray() {
-        return array;
+    public boolean add(int index, String value) {
+        if (index < 0 || index > size) {
+            return false;
+        }
+
+        if (size == array.length) {
+            extendArray();
+        }
+
+        // Зсув елементів вправо
+        for (int i = size - 1; i >= index; i--) {
+            array[i + 1] = array[i];
+        }
+
+        array[index] = value;
+        size++;
+        return true;
     }
 
-    public int getSize() {
-        return size;
+    public boolean add(String value) {
+        return add(size, value);
+    }
+
+    public boolean delete(int index) {
+        if (index < 0 || index >= size) {
+            return false;
+        }
+
+        // Зсув елементів вліво
+        for (int i = index; i < size - 1; i++) {
+            array[i] = array[i + 1];
+        }
+
+        array[size - 1] = null;
+        size--;
+        return true;
+    }
+
+    public boolean delete(String value) {
+        for (int i = 0; i < size; i++) {
+            if (array[i].equals(value)) {
+                return delete(i);
+            }
+        }
+        return false;
+    }
+
+    public String get(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        return array[index];
+    }
+
+    private void extendArray() {
+        String[] newArray = new String[array.length * 2];
+        System.arraycopy(array, 0, newArray, 0, size);
+        array = newArray;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-        DynamicArray array = new DynamicArray();
-        Methods arrayMethods = new Methods();
-        arrayMethods.add(array, "Array");
-        arrayMethods.add(array, "Task");
-        System.out.println(arrayMethods.get(array, 0));
-        System.out.println(arrayMethods.get(array, 1));
-        arrayMethods.delete(array, "Array");
-        System.out.println(arrayMethods.get(array, 0));
-
+        DynamicArray dArray = new DynamicArray();
+        dArray.add("Array");
+        dArray.add("Task");
+        System.out.println(dArray.get(0));
+        System.out.println(dArray.get(1));
+        dArray.delete("Array");
+        System.out.println(dArray.get(0));
     }
 }
